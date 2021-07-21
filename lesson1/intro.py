f01a02e01891f784dff96de86edc52e0bfd54a58
Peter Davison-Reiber <pdavisonreiber@users.noreply.github.com>
2021-07-21 16:59:50 -0600

    Edited intro.py

--- lesson1/intro.py
+++ lesson1/intro.py
@@ -1,8 +1,11 @@
 tprint("Lesson  1")
 print("Welcome to Lesson 1. In this lesson you will learn about input, output and variables.")
-print("👈 On the sidebar on the left you will see the tasks under lesson 1. In each file, you will find instructions to read and code to edit. You can run the code in each task by entering its number below.")
+print("\n👈 On the sidebar on the left you will see folder called lesson 1.")
+print("In this folder you will find one file for each task.")
+print("Read the instructions and edit the code accordingly.")
+print("When you are ready to run your code, enter the number of the task below.")
 
-choice = input("Enter the number of the task you would like to run: ")
+choice = input("\nEnter the number of the task you would like to run: ")
 
 currentLesson = db["currentLesson"]
 path = f"./lesson{currentLesson}/task{choice}.py"
